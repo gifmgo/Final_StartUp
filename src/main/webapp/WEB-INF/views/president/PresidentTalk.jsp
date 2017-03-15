21980a24011bda23b75b2ebbacd35fcccdc22100
박성준 <stpark89@naver.com>
2017-03-16 02:16:10 +0900

    대선후보 관련 글 삭제 완료 씨발

--- src/main/webapp/WEB-INF/views/president/PresidentTalk.jsp
+++ src/main/webapp/WEB-INF/views/president/PresidentTalk.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <div class="container">
 	<br/><br/>
 	<div class="row">
@@ -26,74 +26,108 @@
     <div class="row">
   	  <div class="col-md-12">
   	  	  <div class="well customWell">
-	  		  <div class="panel panel-warning">
-			      <div class="panel-heading">안희정이 되야합니다.<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">좋아요 좋아요</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">문재인이 되야죠<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">그쵸</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">안철수요 <span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">철수형</div>
-			  </div>
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">안희정이 되야합니다.<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">좋아요 좋아요</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">문재인이 되야죠<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">그쵸</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">안철수요 <span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">철수형</div>
-			  </div>
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">안희정이 되야합니다.<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">좋아요 좋아요</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">문재인이 되야죠<span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">그쵸</div>
-			  </div>
-			  
-			  <div class="panel panel-warning">
-			      <div class="panel-heading">안철수요 <span class="pull-right">2016-02-15</span></div>
-			      <div class="panel-body">철수형</div>
-			  </div>
+			  <c:choose>
+			  	<c:when test="${comment_List != null}">
+			  		<c:forEach var="comment" items="${comment_List}">
+						<c:choose>
+							<c:when test="${comment.loginPw == null}">
+								<div class="panel panel-default">
+						  			<div class="panel-heading" style="color:#fff;background-color:${comment.color}">${comment.title}<em>(${comment.empNm} / ${comment.polyNm})</em> <span class="pull-right">${comment.writeDate}<input type="hidden" value="${comment.presidentTalk_seq}"><i class="material-icons" style="font-size:15px" data-toggle="modal" data-target="#talkModal" onclick="deleteComment(this)">delete</i></span></div>
+						  			<div class="panel-body">${comment.comments}</div>
+						  		</div>
+					  		</c:when>
+					  		<c:otherwise>
+					  			<div class="panel panel-default">
+						  			<div class="panel-heading" style="color:#fff;background-color:${comment.color}">${comment.title}<em>(${comment.empNm} / ${comment.polyNm})</em> <span class="pull-right">${comment.writeDate}<input type="hidden" value="${comment.presidentTalk_seq}"><i class="material-icons" style="font-size:15px" onclick="deleteCommentUser(this);">delete</i></span></div>
+						  			<div class="panel-body">${comment.comments}</div>
+						  		</div>
+					  		</c:otherwise>
+				  		</c:choose>
+			  		</c:forEach>
+			  	</c:when>
+			  	<c:otherwise>
+			  		<label>죄송합니다. 잠시후 이용해주세요.</label>
+			  	</c:otherwise>
+			  </c:choose>
 		  </div>
 	  </div>
 	</div>
 </div>  
+<br/>
+<div class="container">
+	<div class="form-group text-center">
+		<hr/>
+		<p class="text-center" style="font-size:22px;">Drag !!</p><br/>
+		<label class="text-center">글을 쓰시려면 대선후보 버튼을 <br/><br/> 아래로 드래그 해주세요 !</label>
+		<hr/>
+	</div>
+
+    <div class="row text-center" id="draggable">
+    	<span class="btn btn-default ui-widget-content" id="문재인" name="더불어민주당">문재인</span>
+    	<span class="btn btn-default ui-widget-content" id="안희정" name="더불어민주당">안희정</span>
+    	<span class="btn btn-default ui-widget-content" id="안철수" name="국민의당">안철수</span>
+    	<span class="btn btn-default ui-widget-content" id="이재명" name="더불어민주당">이재명</span>
+    	<span class="btn btn-default ui-widget-content" id="유승민" name="바른정당">유승민</span>
+  	</div>
+  <div class="row">
+  	<div class="col-md-12 text-center">
+  		<br/>
+  		<div class="well customWell ui-widget-header" id="droppable">
+  			<span><i class="fa fa-pencil-square-o" style="font-size:36px"></i></span><br/>
+  			<p>원하시는 의원의 버튼을 드래그 해주시면</p><br/>
+  			<p>글을 입력하실 수 있습니다 !</p>
+  		</div>
+  		<br/>
+  	</div>
+  </div>
+</div>  
+<div id="writeWell" class="container text-center">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="well">
+				<span><i class="material-icons" style="font-size:36px">assignment_late</i></span><br/>
+				<label style="font-size:22px;">대선후보를 드래그 하시면<br/> 입력폼이 나옵니다 !! </label>
+				<input type="hidden" id="loginNick" value="${sessionScope.id}">
+			</div>
+		</div>
+	</div>
+</div>  
+
 
 <div class="container">
-  <h3 class="text-center">Contact</h3>
-  <p class="text-center"><em>We love our fans!</em></p>
-  <br/>
-  <div class="row">
-    <div class="col-md-offset-2 col-md-8">
-      <div class="row">
-        <div class="col-sm-6 form-group">
-          <input class="form-control" id="name" name="name" placeholder="닉네임" type="text" required>
+	<div class="modal fade" id="talkModal" role="dialog" style="z-index:999999;">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <form class="form-horizontal">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">게시글 삭제</h4>
         </div>
-        <div class="col-sm-6 form-group">
-          <input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>
+        <div class="modal-body">
+        	<input type="hidden" id="hiddenSeq">  
+         	<div class="form-group">
+			    <label class="control-label col-sm-2" for="email">닉네임</label>
+			    <div class="col-sm-8">
+			      <input type="text" class="form-control" id="modal_nickName" placeholder="ex)홍길동">
+			    </div>
+			  </div>
+			  <div class="form-group">
+			    <label class="control-label col-sm-2" for="pw">비밀번호</label>
+			    <div class="col-sm-8"> 
+			      <input type="password" class="form-control" id="modal_pw" placeholder="Enter password">
+			    </div>
+			 </div>
         </div>
-      </div>
-      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea>
-      <br>
-      <div class="row">
-        <div class="col-md-12 form-group">
-          <button class="btn pull-right" type="submit">글등록</button>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" id="modalDelBtn">글삭제</button>
+          <button type="button" class="btn btn-info" data-dismiss="modal">닫기</button>
         </div>
+        </form>
       </div>
     </div>
   </div>
-</div>    
+</div>
+
+
---
+++ src/main/webapp/president/President_0316_02.js
@@ -0,0 +1,445 @@
+/**
+ * 
+ */
+
+//투표 결과 보기
+function showVoteDetail(){
+	$.ajax({
+		url : "voteDetailAjax.do",
+		success : function(data){
+			if(data.list != null){
+				
+				var table = '<table class="table"><thead><tr><th class="text-center">No.</th><th class="text-center">이름</th><th class="text-center">정당</th><th class="text-center">투표수</th></thead><tbody>';
+				$.each(data.list, function(index,obj){
+					if(obj.jungDang.length > 7){
+						obj.jungDang = obj.jungDang.substring(0,6);
+					}
+					table += '<tr><td class="text-center">'+(index+1)+'</td><td class="text-center">'+obj.name+'</td><td class="text-center">'+obj.jungDang+'</td><td class="text-center">'+obj.presidentCount+'</td></tr>';
+				});
+					table += '</tbody></table>';
+					$('#resultVote').html(table);
+				
+			}else{
+				alert("죄송합니다. 잠시후 이용해주세요");
+			}
+		}
+	});
+}
+
+//투표 버튼 클릭시 >> 로그인 했는지 체크 
+function voteFunc(){
+	$.ajax({
+		url : "loginCheck.do",
+		success : function(data){
+			if(data.result == 0){
+				alert(data.message);
+			}else{
+				dbVoteFunc();
+			}
+		}
+	});
+}
+
+//로그인 되어있을 때
+function dbVoteFunc(){
+	
+	var chk = $(':radio[name="optradio"]:checked').val();
+	
+	$.ajax({
+		url : "dbVoteStart.do",
+		data : {
+			chk : chk
+		},
+		success : function(data){
+			
+			switch (data.dbResult) {
+			case 0:
+				alert(data.message);
+				break;
+			case 1 :
+				alert(data.message);
+				break;
+			case 2:
+				alert(data.message);
+				break;
+			
+			case 3:
+				alert(data.message);
+				break;
+				
+			default:
+				alert("죄송합니다. 잠시후 이용해주세요");
+				break;
+			}
+		}
+	});
+}
+
+//새챁 띄우는 전역 변수
+
+//트위터
+function twitterFun(tag){
+	var $btn = $(tag);
+	var url = $btn.attr("id");
+	var ck = checkURL(url);
+	if(ck == true){
+	var openNewWindow = window.open("about:blank");
+	openNewWindow.location.href=url;
+	}
+}
+
+//페이스북
+function FaceBookFun(tag){
+	var $btn = $(tag);
+	var url = $btn.attr("id");
+	var openNewWindow = window.open("about:blank");
+	openNewWindow.location.href=url;
+}
+
+//홈페이지
+function HomePageFunc(tag){
+	var $btn = $(tag);
+	var url = $btn.attr("id");
+	var openNewWindow = window.open("about:blank");
+	openNewWindow.location.href=url;
+}
+
+//삭제 아이콘 클릭시 
+function deleteComment(icon){
+	var seq_Val = $(icon).prev().val();
+	$('#hiddenSeq').val('');
+	$('#hiddenSeq').val(seq_Val);
+}
+
+//삭제 아이콘 클릭시 >> 로그인한 유저일때
+function deleteCommentUser(icon){
+	var seq_Val = $(icon).prev().val();
+}
+
+function loginFunc(){
+	location.href="LoginP.do";
+}
+
+
+//글쓰기 폼 만듬 >> 로그인 안했을때 
+function makeForm(empNm, polyNm){
+
+	var sc_writeForm = '';
+	switch (empNm) {
+	case '문재인':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="nickName" placeholder="닉네임" type="text" required>';
+					sc_writeForm += '</div>';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';
+				sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-12 form-group">';
+				sc_writeForm += '<input class="form-control" id="title" name="title" placeholder="제목" type="text" required">';
+				sc_writeForm += '</div></div>';
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+			sc_writeForm += '<div class="row"><div class="col-md-12 form-group"><button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button></div></div>';
+			sc_writeForm += '</div></div></form>';
+		break;
+	case '안희정':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="nickName" placeholder="닉네임" type="text" required>';
+					sc_writeForm += '</div>';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';
+				sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-12 form-group">';
+				sc_writeForm += '<input class="form-control" id="title" name="title" placeholder="제목" type="text" required">';
+				sc_writeForm += '</div></div>';
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+			sc_writeForm += '<div class="row"><div class="col-md-12 form-group"><button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button></div></div>';
+			sc_writeForm += '</div></div></form>';
+		break;
+	
+	case '안철수':	
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#1abc9c">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #1abc9c; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#1abc9c;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="nickName" placeholder="닉네임" type="text" required>';
+					sc_writeForm += '</div>';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';
+				sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-12 form-group">';
+				sc_writeForm += '<input class="form-control" id="title" name="title" placeholder="제목" type="text" required">';
+				sc_writeForm += '</div></div>';
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+			sc_writeForm += '<div class="row"><div class="col-md-12 form-group"><button class="btn pull-right type="submit" style="background-color:#1abc9c; color:white;">글등록</button></div></div>';
+			sc_writeForm += '</div></div></form>';
+		break;
+	
+	case '이재명':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="nickName" placeholder="닉네임" type="text" required>';
+					sc_writeForm += '</div>';
+					sc_writeForm += '<div class="col-sm-6 form-group">';
+						sc_writeForm += '<input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';
+				sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-12 form-group">';
+				sc_writeForm += '<input class="form-control" id="title" name="title" placeholder="제목" type="text" required">';
+				sc_writeForm += '</div></div>';
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+			sc_writeForm += '<div class="row"><div class="col-md-12 form-group"><button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button></div></div>';
+			sc_writeForm += '</div></div></form>';
+		break;	
+		
+	case '유승민':	
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#3498db">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #3498db; background-color:#fff;">'
+		sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#3498db;">'+polyNm+'</em></p></h3><hr/><br/>';
+		sc_writeForm += '<div class="row">';
+		sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-6 form-group">';
+					sc_writeForm += '<input class="form-control" id="name" name="nickName" placeholder="닉네임" type="text" required>';
+				sc_writeForm += '</div>';
+				sc_writeForm += '<div class="col-sm-6 form-group">';
+					sc_writeForm += '<input class="form-control" id="pw" name="pw" placeholder="비밀번호" type="password" required>';
+				sc_writeForm += '</div>';
+			sc_writeForm += '</div>';
+			sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-sm-12 form-group">';
+			sc_writeForm += '<input class="form-control" id="title" name="title" placeholder="제목" type="text" required">';
+			sc_writeForm += '</div></div>';
+		sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+		sc_writeForm += '<div class="row"><div class="col-md-12 form-group"><button class="btn pull-right type="submit" style="background-color:#3498db; color:white;">글등록</button></div></div>';
+		sc_writeForm += '</div></div></form>';
+		break;	
+		
+	default:
+		break;
+	}
+	
+	$('#writeWell').empty();
+	$('#writeWell').html(sc_writeForm);
+}
+
+
+//로그인 했을 때 사용하는 폼
+function makeMemberForm(empNm, polyNm){
+	var sc_writeForm = '';
+	switch (empNm) {
+	case '문재인':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-12 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="title" placeholder="제목" type="text" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';	
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-md-12 form-group">';
+						sc_writeForm += '<button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button>';
+					sc_writeForm +=	'</div>';
+				sc_writeForm +=	'</div>';
+			sc_writeForm += '</div></div></div></form>';
+		break;
+	case '안희정':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-12 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="title" placeholder="제목" type="text" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';	
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-md-12 form-group">';
+						sc_writeForm += '<button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button>';
+					sc_writeForm +=	'</div>';
+				sc_writeForm +=	'</div>';
+			sc_writeForm += '</div></div></div></form>';
+		break;
+	
+	case '안철수':	
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#1abc9c">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #1abc9c; background-color:#fff;">'
+		sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#1abc9c;">'+polyNm+'</em></p></h3><hr/><br/>';
+		sc_writeForm += '<div class="row">';
+			sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-sm-12 form-group">';
+					sc_writeForm += '<input class="form-control" id="name" name="title" placeholder="제목" type="text" required>';
+				sc_writeForm += '</div>';
+			sc_writeForm += '</div>';	
+		sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-md-12 form-group">';
+					sc_writeForm += '<button class="btn pull-right type="submit" style="background-color:#1abc9c; color:white;">글등록</button>';
+				sc_writeForm +=	'</div>';
+			sc_writeForm +=	'</div>';
+		sc_writeForm += '</div></div></div></form>';
+		break;
+	
+	case '이재명':
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#2980b9">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #2980b9; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#2980b9;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-12 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="title" placeholder="제목" type="text" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';	
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-md-12 form-group">';
+						sc_writeForm += '<button class="btn pull-right type="submit" style="background-color:#2980b9; color:white;">글등록</button>';
+					sc_writeForm +=	'</div>';
+				sc_writeForm +=	'</div>';
+			sc_writeForm += '</div></div></div></form>';
+		break;	
+		
+	case '유승민':	
+		sc_writeForm += '<form method="POST">';
+		sc_writeForm += '<input type="hidden" name="color" value="#3498db">';
+		sc_writeForm += '<input type="hidden" name="empNm" value='+empNm+'>';
+		sc_writeForm += '<input type="hidden" name="polyNm" value='+polyNm+'>';
+		sc_writeForm += '<div class="well" style="border:4px solid #3498db; background-color:#fff;">'
+			sc_writeForm += '<h3 class="text-center">'+empNm+'<p><br/><em style="color:#3498db;">'+polyNm+'</em></p></h3><hr/><br/>';
+			sc_writeForm += '<div class="row">';
+				sc_writeForm += '<div class="col-md-offset-2 col-md-8">';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-sm-12 form-group">';
+						sc_writeForm += '<input class="form-control" id="name" name="title" placeholder="제목" type="text" required>';
+					sc_writeForm += '</div>';
+				sc_writeForm += '</div>';	
+			sc_writeForm += '<textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" maxlength="500"></textarea><br/>';
+				sc_writeForm += '<div class="row">';
+					sc_writeForm += '<div class="col-md-12 form-group">';
+						sc_writeForm += '<button class="btn pull-right type="submit" style="background-color:#3498db; color:white;">글등록</button>';
+					sc_writeForm +=	'</div>';
+				sc_writeForm +=	'</div>';
+			sc_writeForm += '</div></div></div></form>';
+		break;	
+		
+	default:
+		break;
+	}
+	
+	$('#writeWell').empty();
+	$('#writeWell').html(sc_writeForm);
+
+}
+
+
+$(document).ready(function(){
+
+	var loginId = $('#loginNick').val();
+	
+	//드래그앤 드롭 하는 스크립트
+	$("#draggable span").draggable({
+		start : function(event, ui){
+			$(this).draggable("option", "revert", true);	
+		}
+	});
+	
+	$("#droppable").droppable({
+    	drop: function( event, ui ) {
+			var dragid = ui.draggable.attr("id");
+			var polyNm = ui.draggable.attr("name");
+			if(loginId == '' || loginId == null){
+				makeForm(dragid, polyNm);
+			}else{
+				makeMemberForm(dragid, polyNm);
+			}
+    	}
+	});
+	
+	$('#modalDelBtn').click(function(){
+		alert("모달 글 삭제2 !");
+		var seq = $('#hiddenSeq').val();
+		var id = $('#modal_nickName').val();
+		var pw = $('#modal_pw').val();
+		
+		$.ajax({
+			url : "deletePresidentTalk.do",
+			data : {
+				seq : seq,
+				id : id,
+				pw : pw
+			},
+			success : function(data){
+				if(data.result > 0){
+					alert("글삭제 성공 !");
+					location.href="PresidentTalk.do";
+				}else{
+					alert("죄송합니다. 잠시후 이용해주세요");
+					location.href="PresidentTalk.do";
+				}
+			}
+		});
+		
+	});
+	
+	
+});
